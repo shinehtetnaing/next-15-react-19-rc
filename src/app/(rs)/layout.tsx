cc5b4e16b231780b04a204db6ea664ebe2a137b6
shinehtetnaing <shinehtetnaing1998@gmail.com>
2024-10-18 09:38:19 +0700

    basic routing, layout & template

---
+++ src/app/(rs)/layout.tsx
@@ -0,0 +1,11 @@
+export default async function RSLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="mx-auto w-full max-w-7xl">
+      <div className="px-4 py-2">{children}</div>
+    </div>
+  );
+}
